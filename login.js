9bfb37b88f7e08368d3be1ae7cfe909dc4682ee4
kccowboy3 <kccowboy3@gmail.com>
2026-07-20 11:59:47 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+const gameText = document.getElementById('game-text2');
+const userInput = document.getElementById('user-input2');
+const submitBtn = document.getElementById('submit-btn2');
+function print(text){
+  game Text.innerText+= text + "\n"
+  game Text.scrollTop = gameText.scrollHeight;
+}
